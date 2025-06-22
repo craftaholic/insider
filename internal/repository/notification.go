45940607cb92082c1eb7c943a9deeda2a94f4691
Tommy Tran Duc Thang <tommytrandt.work@gmail.com>
2025-06-22 13:30:09 +0700

    feat(send-notification): implement logic  for send notification

--- internal/repository/notification.go
+++ internal/repository/notification.go
@@ -2,22 +2,71 @@ package repository
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 
 	"github.com/craftaholic/insider/internal/domain"
+	"github.com/craftaholic/insider/internal/shared/log"
+	"github.com/craftaholic/insider/internal/utils"
+	"github.com/go-resty/resty/v2"
 )
+
+// NotificationRequest structs is for parsing request.
+type NotificationRequest struct {
+	To      string `json:"to"`
+	Content string `json:"content"`
+}
+
+// NotificationResponse structs is for parsing response.
+type NotificationResponse struct {
+	Message   string `json:"message"`
+	MessageID string `json:"messageId"`
+}
 
 type NotificationService struct {
 	apiKey   string
 	endPoint string
+	client   *resty.Client
 }
 
 func NewNotificationService(apiKey string, endPoint string) domain.NotificationService {
 	return &NotificationService{
 		apiKey:   apiKey,
 		endPoint: endPoint,
+		client:   resty.New(),
 	}
 }
 
 func (ns *NotificationService) SendNotification(c context.Context, message domain.Message) (string, error) {
-	return "", nil
+	logger := log.FromCtx(c).WithFields("action", "Sending notification")
+
+	notificationRequest := NotificationRequest{
+		To:      message.PhoneNumber,
+		Content: message.Content,
+	}
+
+	body, err := json.Marshal(notificationRequest)
+	if err != nil {
+		logger.Error("Can't marshal notification request object to byte")
+		return "", err
+	}
+
+	response, webhookErr := utils.Post(ns.client, ns.apiKey, body, ns.endPoint)
+	if webhookErr != nil {
+		logger.Error("Error sending notification", "error", err)
+		return "", webhookErr
+	}
+
+	var notificationResponse NotificationResponse
+	err = json.Unmarshal(response.Body(), &notificationResponse)
+	if err != nil {
+		logger.Error("Error sending notification", "error", err)
+	}
+
+	if notificationResponse.Message != "Accepted" {
+		logger.Error("Error sending notification", "response for webhook", notificationResponse.Message)
+		return "", errors.New("Error sending notification " + "response for webhook " + notificationResponse.Message)
+	}
+
+	return notificationResponse.MessageID, nil
 }
